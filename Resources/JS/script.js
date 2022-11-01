398fef1f8d148135f0592e47126fb8363378984d
Vitaliy Rodyna <ukrdao@gmail.com>
2022-11-01 06:21:29 +0200

    // Task input event listener: addme.addEventListener("click", function () {   if (task.value === "") {     addme.classList.add("animate__shakeX");     setTimeout(function () {       addme.classList.remove("animate__shakeX");     }, 1000);   } else {     tasks.push(task.value);     addtask(tasks.lastIndexOf(task.value));     task.value = "";   } });

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -6,3 +6,52 @@ const clear = document.querySelector(".clear");
 let task = document.getElementById("task");
 // Array decleration:
 let tasks = new Array();
+
+// Task input event listener:
+addme.addEventListener("click", function () {
+    if (task.value === "") {
+      addme.classList.add("animate__shakeX");
+      setTimeout(function () {
+        addme.classList.remove("animate__shakeX");
+      }, 1000);
+    } else {
+      tasks.push(task.value);
+      addtask(tasks.lastIndexOf(task.value));
+      task.value = "";
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
