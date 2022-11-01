b7ff58145d1881b99af5b047baf603538553c46b
Vitaliy Rodyna <ukrdao@gmail.com>
2022-11-01 06:20:39 +0200

    "use strict"; // Variable Decleration: const addme = document.querySelector(".add-me"); const display = document.querySelector(".task-display"); const clear = document.querySelector(".clear"); let task = document.getElementById("task"); // Array decleration: let tasks = new Array();

---
+++ Resources/JS/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Variable Decleration:
+const addme = document.querySelector(".add-me");
+const display = document.querySelector(".task-display");
+const clear = document.querySelector(".clear");
+let task = document.getElementById("task");
+// Array decleration:
+let tasks = new Array();
